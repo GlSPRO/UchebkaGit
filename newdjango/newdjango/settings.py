ddf9a251f2fd738a32a68a5a7092dd82abc74eb1
GISPro <gleboss2517@yandex.ru>
2025-06-21 08:06:36 +0300

    COMmIT

--- newdjango/newdjango/settings.py
+++ newdjango/newdjango/settings.py
@@ -79,12 +79,12 @@ WSGI_APPLICATION = 'newdjango.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'HOST': '',
-        'PORT': '3306',
-        'NAME' : 'Uchebka',
-        'USER': '',
-        'PASSWORD': '',
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+        'NAME' : 'p3_django',
+        'USER': 'postgres',
+        'PASSWORD': '1',
     }
 }
 
